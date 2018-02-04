1e0337a0e653ae91b8d9ee8dc8667966f04fab98
Aaron Curry <aaron@aaroncurry.net>
2018-02-03 22:09:02 -0600

    Add some os x settings

--- setup.rb
+++ setup.rb
@@ -4,12 +4,18 @@ require './class_files/configuration_list_reader.rb'
 require './class_files/application_installer.rb'
 require './class_files/manual_task_reporter.rb'
 require './class_files/application_opener.rb'
+require './class_files/os_x_configurator.rb'
 
 APP_CONFIG_FILE = "./configs/applications.json"
 MANUAL_STEPS_FILE = "./configs/manual_steps.json"
+OS_X_SETTINGS_FILE = "./configs/os_x_settings.json"
 
 # Just get admin priveleges from the get; this may or may not help
 system("sudo -v")
+
+os_x_configs = ConfigurationListReader.from_file(OS_X_SETTINGS_FILE, 'os_x_settings').configurations
+os_x_configurator = OSXConfigurator.new(os_x_configs)
+os_x_configurator.configure!
 
 application_list = ConfigurationListReader.from_file(APP_CONFIG_FILE, 'applications').configurations
 application_installer = ApplicationInstaller.new(application_list)
@@ -21,3 +27,5 @@ manual_task_reporter.report!
 
 app_opener = ApplicationOpener.new(application_list)
 app_opener.open_apps!
+
+
