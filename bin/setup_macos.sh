f77f89a5846f4ed46e75315157fafb2a1b417a74
Aaron Curry <aaron@aaroncurry.net>
2019-07-16 14:34:28 -0700

    Fix up the system for macOS configuration

---
+++ bin/setup_macos.sh
@@ -0,0 +1,78 @@
+#!/bin/sh -e
+
+# Set up all of the preferences
+
+do_keyboard_setup() {
+  echo "Setting up keyboard preferences..."
+
+  # Fast key repeats
+  defaults write -g InitialKeyRepeat -int 7
+  defaults write -g KeyRepeat -int 0
+}
+
+do_touchbar_setup() {
+  echo "Fixing the touchbar..."
+  # Only show the regular control strip
+  defaults write com.apple.touchbar.agent PresentationModeGlobal fullControlStrip
+  # Show function keys on fn press
+  defaults write com.apple.touchbar.agent PresentationModeFnModes -dict-add fullControlStrip functionKeys
+}
+
+do_dock_setup() {
+  echo "Setting up the dock..."
+
+	# Only show active things in the dock
+	defaults write com.apple.dock static-only -bool true
+	# Autohide the dock
+	defaults write com.apple.dock autohide -bool true
+	# Put it on the left
+  defaults write com.apple.Dock orientation -string "left"
+
+    # Hot corners
+  # Possible values:
+  #  0: no-op
+  #  2: Mission Control
+  #  3: Show application windows
+  #  4: Desktop
+  #  5: Start screen saver
+  #  6: Disable screen saver
+  #  7: Dashboard
+  # 10: Put display to sleep
+  # 11: Launchpad
+  # 12: Notification Center
+  defaults write com.apple.dock wvous-bl-corner -int 5
+  defaults write com.apple.dock wvous-bl-modifier -int 0
+}
+
+do_trackpad_setup() {
+  echo "Setting up the trackpad..."
+
+  # Enable one-click taps
+  defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+  defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+  defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+  defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+  # Trackpad: enable right click with two fingers
+  defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+  defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool true
+  defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+  defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool true
+}
+
+do_kill_running_apps()  {
+  killall Dock
+  killall ControlStrip
+}
+
+# Wipe everything back to factory defaults
+do_config_resets() {
+  # TODO - what about everything else?
+  defaults delete com.apple.dock
+}
+
+do_keyboard_setup
+do_trackpad_setup
+do_dock_setup
+do_touchbar_setup
+do_kill_running_apps
--- setup.rb
+++ setup.rb
@@ -4,7 +4,6 @@ require './class_files/configuration_list_reader.rb'
 require './class_files/application_installer.rb'
 require './class_files/manual_task_reporter.rb'
 require './class_files/application_opener.rb'
-require './class_files/os_x_configurator.rb'
 
 APP_CONFIG_FILE = "./configs/applications.json"
 MANUAL_STEPS_FILE = "./configs/manual_steps.json"
@@ -13,9 +12,8 @@ OS_X_SETTINGS_FILE = "./configs/os_x_settings.json"
 # Just get admin priveleges from the get; this may or may not help
 system("sudo -v")
 
-os_x_configs = ConfigurationListReader.from_file(OS_X_SETTINGS_FILE, 'os_x_settings').configurations
-os_x_configurator = OSXConfigurator.new(os_x_configs)
-os_x_configurator.configure!
+system("./bin/setup_macos.sh")
+puts "Done configuring OS X"
 
 application_list = ConfigurationListReader.from_file(APP_CONFIG_FILE, 'applications').configurations
 application_installer = ApplicationInstaller.new(application_list)
