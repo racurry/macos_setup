7353510c6e4c313c876a438e65546d9c223abc65
Aaron Curry <aaron@aaroncurry.net>
2020-04-18 15:53:40 -0500

    Add small script for the kids macs

---
+++ bin/kids_mac.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+
+ # A one-off for use on kids' computers
+
+ brew cask install chrome
+ brew cask install openemu
+ brew cask install obs
+ brew cask install spotify
+ brew cask install steam
+ brew cask install paintbrush
+ brew cask install hazel
