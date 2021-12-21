4662379f8d3a24131d3ec19e2ab7bc4848b596fa
Aaron Curry <aaron@aaroncurry.net>
2021-12-21 09:50:53 -0600

    Fix up kids script and my brew apps

--- bin/kids_mac.sh
+++ bin/kids_mac.sh
@@ -1,11 +1,12 @@
  #!/bin/bash
 
  # A one-off for use on kids' computers
-
- brew cask install google-chrome
- brew cask install openemu
- brew cask install obs
- brew cask install spotify
- brew cask install steam
- brew cask install paintbrush
- brew cask install hazel
+brew install caffeine
+brew install google-chrome
+brew install hazel
+brew install lastpass
+brew install obs
+brew install openemu
+brew install paintbrush
+brew install spotify
+brew install steam
