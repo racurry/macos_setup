58bb7ae0b4b9d2835c51a8e43014a213493cc26b
Aaron Curry <aaron@aaroncurry.net>
2021-12-21 09:59:02 -0600

    eh

--- bin/kids_mac.sh
+++ bin/kids_mac.sh
@@ -1,19 +1,12 @@
  #!/bin/bash
 
  # A one-off for use on kids' computers
-# brew install caffeine
-# brew install google-chrome
-# brew install hazel
-# brew install lastpass
-# brew install obs
-# brew install openemu
-# brew install paintbrush
-# brew install spotify
-# brew install steam
-
-
-APPS=('caffeine', 'google-chrome', 'hazel', 'lastpass', 'obs', 'openemu', 'paintbrush', 'spotify', 'steam')
-for app in "${APPS[@]}"
-do
-  brew install $app
-done
+brew install caffeine
+brew install google-chrome
+brew install hazel
+brew install lastpass
+brew install obs
+brew install openemu
+brew install paintbrush
+brew install spotify
+brew install steam
